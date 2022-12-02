26a02adcff62789fa4ab86c13ff31fbfb4e1e9a8
minhdo6487 <minhdo6487@gmail.com>
2022-12-02 23:15:57 +0700

    main - try fix cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -45,7 +45,7 @@ async function bootstrap() {
       }
     },
   };
-  app.use(cors(corsOptions));
+  app.use(cors());
   app.useGlobalFilters(new ErrorFilter());
   await app.listen(config.PORT);
   logger.log(`Listening on port ${config.PORT}.`);
